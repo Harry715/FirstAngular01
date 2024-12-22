41a1997064bf65daf740d46421ec29f999960a29
xiao20020715 <xiao20020715@gmail.com>
2024-12-22 20:45:26 +0800

    1

--- project01/src/app/app-routing.module.ts
+++ project01/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component'
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '123', component: HomeComponent },
   { path: 'login', component: LoginComponent }
 ];
 
