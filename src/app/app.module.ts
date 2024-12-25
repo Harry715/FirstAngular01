5fd50193fd6eb56ddb53a619da9c7f8f24b70f3c
Blithe Hsiao 蕭浩倫 <003652@transglobe.com.tw>
2024-12-25 17:21:54 +0800

    Api Bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +10,9 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BrowserModule,
+    FormsModule
   ],
   exports: [RouterModule]
 })
